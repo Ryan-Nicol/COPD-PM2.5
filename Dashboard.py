39e519df3b783285a4a755c5cbf470fa9a655781
Ryan-Nicol <rnn5@aber.ac.uk>
2026-08-21 15:14:33 +0100

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -14,7 +14,6 @@ copd = pd.read_csv('COPD-PM2_5.csv')
 
 
 st.write("### 5. Scatter Chart")
-st.scatter_chart(copd,
-                 x=date,
-                 y='count',
-                 color='pm2_5')
+st.scatter_chart(x=copd['date'],
+                 y=copd['count'],
+                 color=copd['pm2_5'])
