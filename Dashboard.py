9fd021407812d19c16266c817886c0f464033de7
Ryan-Nicol <rnn5@aber.ac.uk>
2026-08-24 14:52:04 +0100

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from typing import List
 import pandas as pd
 import streamlit as st
+import plotly.graph_objects as go
 
 st.set_page_config(
     page_title="PM2.5 and hospitalisations in Chiang Mai",
