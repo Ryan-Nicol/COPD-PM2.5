2ed9af3abc60f0c7f34ce6c41736912498e0d44b
Ryan-Nicol <rnn5@aber.ac.uk>
2026-08-24 17:29:37 +0100

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -10,16 +10,9 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded")
 
-copd = pd.read_csv('COPD-PM2_5.csv', sep='comma')
+copd = pd.read_csv('Full_data.xlsx', sep='comma')
 
 st.write("### 5. Scatter Chart")
-from numpy.random import default_rng as rng
-
-df = pd.DataFrame(rng(0).standard_normal((20, 3)), columns=["a", "b", "c"])
-
-st.bar_chart(df)
-
-
 st.scatter_chart(copd,
                  x='date',
                  y='count',
