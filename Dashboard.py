8b048b587df6b3ad48aaa3207073a2d818b26b0d
Ryan-Nicol <rnn5@aber.ac.uk>
2026-08-24 14:36:22 +0100

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -12,21 +12,6 @@ st.set_page_config(
 
 copd = pd.read_csv('COPD-PM2_5.csv', sep='comma')
 
-from numpy.random import default_rng as rng
-
-hist_data = [
-    rng(0).standard_normal(200) - 2,
-    rng(1).standard_normal(200),
-    rng(2).standard_normal(200) + 2,
-]
-group_labels = ["Group 1", "Group 2", "Group 3"]
-
-fig = ff.create_distplot(
-    hist_data, group_labels, bin_size=[0.1, 0.25, 0.5]
-)
-
-st.plotly_chart(fig)
-
 st.write("### 5. Scatter Chart")
 st.scatter_chart(copd,
                  x='date',
