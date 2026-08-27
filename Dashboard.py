86a025cafceb677d86600205e9371707319586bc
Ryan-Nicol <rnn5@aber.ac.uk>
2026-08-27 17:21:06 +0100

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -12,6 +12,24 @@ st.write("# PM2.5 and hospitalisations in Chiang Mai")
 
 col1, col2 = st.columns([3,3])
 
+selectedf = st.selectbox(
+        "Select a variable for forecasted data",
+        options=['pm10', 'pm2_5_y','carbon_monoxide','carbon_dioxide',
+                                'nitrogen_dioxide','sulphur_dioxide','ozone','count'],
+        index=1
+    )
+fig, ax1 = plt.subplots(figsize=(8, 8))
+ax2 = ax1.twinx()
+
+ax1.plot(copd_f, date, count)
+ax2.plot(copd_f, date, pm2_5_y)
+
+
+col1.line_chart(copd_f,
+                 y=selectedf,
+                 x='date',
+                 color='blue')
+
 col1.line_chart(copd,
                  x="date",
                  y='count',
@@ -38,13 +56,4 @@ col2.scatter_chart(copd,
                  x=selected2,
                  y='count',
                  color='blue')
-selectedf = st.selectbox(
-        "Select a variable for forecasted data",
-        options=['pm10', 'pm2_5_y','carbon_monoxide','carbon_dioxide',
-                                'nitrogen_dioxide','sulphur_dioxide','ozone','count'],
-        index=1
-    )
-col1.line_chart(copd_f,
-                 y=selectedf,
-                 x='date',
-                 color='blue')
+
