3536345490c425a3d6218269a6475e9b2d5ee3e2
Ryan-Nicol <rnn5@aber.ac.uk>
2026-09-01 16:52:40 +0100

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -7,7 +7,7 @@ copd_f = pd.read_csv('COPD-PM2_5_F_processed.csv', sep=',', parse_dates=[0], dat
 st.set_page_config(
     page_title="PM2.5 and hospitalisations in Chiang Mai",
     layout="wide")
-st.write("# PM2.5 and hospitalisations in Chiang Mai")
+
 
 
 col1, col2 = st.columns([1,1])
