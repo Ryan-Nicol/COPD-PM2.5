d677f7f2831eb6617106c7b8ef7b324c23b464f0
Ryan-Nicol <rnn5@aber.ac.uk>
2026-09-01 15:47:24 +0100

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -31,7 +31,7 @@ col2.line_chart(copd_f,
                  color='violet')
 
 st.write("##### The number of historical and forecasted hospitalisations between 2023-2025 and the next 6 days, coloured by PM2.5 level")
-fig = go.Figure([go.Scatter(x=copd['date'], y=copd['count']),x_label='Date', y_label='Hospitalisation Count'],color= copd['pm2_5'])
+fig = go.Figure([go.Scatter(x=copd['date'], y=copd['count']), x_label=='Date', y_label='Hospitalisation Count'],color= copd['pm2_5'])
 
 fig.update_xaxes(nticks=5)
 
