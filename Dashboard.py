a199cb18437889a5c7006241593c01b748a918a1
Ryan-Nicol <rnn5@aber.ac.uk>
2026-09-03 13:33:46 +0100

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -45,7 +45,7 @@ col3, col4 = st.columns([1,1])
 col4.write("##### Interaction between both types of PM and the hospitalisation count")
 selected = col4.selectbox(
         "Select a variable for the x axis",
-        options=['pm2_5', 'pm10'],
+        options=['pm2_5_val', 'pm10'],
         index=0
     )
 col4.scatter_chart(copd,
@@ -56,11 +56,11 @@ col4.scatter_chart(copd,
 
 col3.write("##### The total number of historical hospitalisations sorted by PM2.5 level")
 col3.bar_chart(copd[0:1095],
-                 x="pm2_5_categories",
+                 x="pm2_5",
                x_label='PM2.5 Categories',
                  y='count',
                y_label='Hospitalisation Count',
-                 color='pm2_5_categories')
+                 color='pm2_5')
 col3.markdown(''' :red[very low = 1-15ppm], :blue[low = 15.1-25ppm], :red[moderate = 25.1-37.5ppm],
             :blue[high = 37.6-75ppm], :green[very unsafe = 75.1-150ppm]''')
 
