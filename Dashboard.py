b5cfe69667253a176661f9bb40dbbcb25168b3d0
Ryan-Nicol <rnn5@aber.ac.uk>
2026-09-03 13:34:30 +0100

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -29,9 +29,9 @@ col2.line_chart(copd_f,
                  color='violet')
 
 st.write("##### The number of historical and forecasted hospitalisations between 2023-2025 and the next 6 days, coloured by PM2.5 level")
-st.scatter_chart(copd,
+st.scatter_chart(copd[0:1095],
                  x="date",
-               x_label='Date(2023-2026)',
+               x_label='Date',
                  y='count',
               y_label='Hospitalisation Count',
                  color= 'pm2_5',
