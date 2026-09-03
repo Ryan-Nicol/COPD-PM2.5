29f519c85970537e169cf1045020864aaa04a1f1
Ryan-Nicol <rnn5@aber.ac.uk>
2026-09-03 12:46:16 +0100

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -35,7 +35,7 @@ st.scatter_chart(copd,
                  y='count',
               y_label='Hospitalisation Count',
                  color= 'pm2_5_categories',
-               width= 'content')
+               width= 'stretch')
 st.markdown(''' :red[very low = 1-15ppm], :blue[low = 15.1-25ppm], :red[moderate = 25.1-37.5ppm],
             :blue[high = 37.6-75ppm], :green[very unsafe = 75.1-150ppm]''')
 
